7cb4b33fccaba62332a92e29f57b66830a4c07d8
Kristijan Delač <kdelac@foi.hr>
2018-05-02 16:24:15 +0200

    Dodan LayoutView

---
+++ Application/EasyLibraryApplication/EasyLibraryApplication.WPF/View/LayoutView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EasyLibraryApplication.WPF.View
+{
+    /// <summary>
+    /// Interaction logic for LayoutView.xaml
+    /// </summary>
+    public partial class LayoutView : Window
+    {
+        public LayoutView()
+        {
+            InitializeComponent();
+        }
+    }
+}
