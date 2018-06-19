9ac1d17a641680dccd79d58fbd529bbcd6eed04e
Kristijan Delač <kdelac@foi.hr>
2018-06-20 00:11:21 +0200

    Dodan interface u klasu LoginViewModel

--- Application/EasyLibraryApplication/EasyLibraryApplication.WPF/ViewModel/LoginViewModel.cs
+++ Application/EasyLibraryApplication/EasyLibraryApplication.WPF/ViewModel/LoginViewModel.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using EasyLibraryApplication.WPF.Annotations;
+using EasyLibraryApplication.WPF.Model;
 
 namespace EasyLibraryApplication.WPF.ViewModel
 {
-    class LoginViewModel
+    class LoginViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
