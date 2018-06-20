66f7a5b052b2f5dfb53844eafe9931baea37b71c
Kristijan Delač <kdelac@foi.hr>
2018-06-20 02:13:05 +0200

    Dodane dvije klase za upravljanje događajima, dodjeljeni im ICommand interfacei, napisana metoda za provjeru korisnika u bazi

---
+++ Application/EasyLibraryApplication/EasyLibraryApplication.WPF/Commands/LoginCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using EasyLibraryApplication.WPF.View;
+using EasyLibraryApplication.WPF.ViewModel;
+
+namespace EasyLibraryApplication.WPF.Commands
+{
+    class LoginCommand : ICommand
+    {
+        private LoginViewModel viewModel;
+
+        public LoginCommand(LoginViewModel vM)
+        {
+            viewModel = vM;
+
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            viewModel.CheckUser();
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
--- Application/EasyLibraryApplication/EasyLibraryApplication.WPF/ViewModel/LoginViewModel.cs
+++ Application/EasyLibraryApplication/EasyLibraryApplication.WPF/ViewModel/LoginViewModel.cs
@@ -5,13 +5,68 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using EasyLibraryApplication.WPF.Annotations;
+using EasyLibraryApplication.WPF.Commands;
 using EasyLibraryApplication.WPF.Model;
+using EasyLibraryApplication.WPF.View;
 
 namespace EasyLibraryApplication.WPF.ViewModel
 {
     class LoginViewModel : INotifyPropertyChanged
     {
+        private LibraryEntities ctx;
+        private Login loginView;
+
+        /// <summary>
+        /// Konstruktor klase 
+        /// </summary>
+        public LoginViewModel(Login login)
+        {
+            ctx = new LibraryEntities();
+            LoginEvent = new LoginCommand(this);
+            RegisterEven = new RegisterCommand();
+            loginView = login;
+            SelectedItem = new User();
+        }
+
+        #region Commands
+
+        public LoginCommand LoginEvent { get; set; }
+        public RegisterCommand RegisterEven { get; set; }
+
+        #endregion
+
+        #region Selected Item
+
+        private User selectedItem;
+
+        public User SelectedItem {
+            get => selectedItem;
+            set
+            {
+                selectedItem = value;
+                OnPropertyChanged(nameof(SelectedItem));
+            }
+        }
+
+        #endregion
+
+        public void CheckUser()
+        {
+            User username = ctx.Users.FirstOrDefault(s => s.Username == SelectedItem.Username);
+            User password = ctx.Users.FirstOrDefault(s => s.PasswordHash == "kdelac123");
+            if (username != null && password != null)
+            {
+                MessageBox.Show($"Bok, {username.Username}");
+                loginView.Close();
+            }
+            else
+            {
+                MessageBox.Show("Ne postoji");
+            }
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         [NotifyPropertyChangedInvocator]
