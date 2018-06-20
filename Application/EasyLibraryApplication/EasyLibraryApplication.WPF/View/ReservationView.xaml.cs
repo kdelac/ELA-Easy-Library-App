794e51ce3760c7345ab782beb6d4903e81728c03
Kristijan Mihaljinac <kristijan1mihaljinac@outlook.com>
2018-06-20 22:10:20 +0200

    Izrađen viewmodel i view za rezervaciju. NIJE dovršeno

--- Application/EasyLibraryApplication/EasyLibraryApplication.WPF/View/ReservationView.xaml.cs
+++ Application/EasyLibraryApplication/EasyLibraryApplication.WPF/View/ReservationView.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using EasyLibraryApplication.WPF.Model;
+using EasyLibraryApplication.WPF.ViewModel;
+
 
 namespace EasyLibraryApplication.WPF.View
 {
@@ -23,6 +26,7 @@ namespace EasyLibraryApplication.WPF.View
         public ReservationView()
         {
             InitializeComponent();
+            this.DataContext = new ReservationViewModel();
         }
     }
 }
---
+++ Application/EasyLibraryApplication/EasyLibraryApplication.WPF/ViewModel/ReservationViewModel.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using EasyLibraryApplication.WPF.Annotations;
+using EasyLibraryApplication.WPF.Model;
+
+namespace EasyLibraryApplication.WPF.ViewModel
+{
+    class ReservationViewModel : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+        public CollectionViewSource LibraryCollectionViewSource { get; private set; }
+        private LibraryEntities ctx;
+
+        private Book selectedBook;
+
+        public Book SelectedBook
+        {
+            get { return selectedBook; }
+            set
+            {
+                selectedBook = value;
+                OnPropertyChanged(nameof(SelectedBook));
+            }
+        }
+
+        #region SelectedLibray
+
+        private Library selectedLibrary;
+
+        public Library SelectedLibrary
+        {
+            get { return selectedLibrary; }
+            set
+            {
+                selectedLibrary = value;
+                OnPropertyChanged(nameof(SelectedLibrary));
+            }
+        }
+
+
+        #endregion
+        public ReservationViewModel(/*Book book*/)
+        {
+            LibraryCollectionViewSource = new CollectionViewSource();
+            LoadData();
+     
+        }
+
+        private void LoadData()
+        {
+            Refresh();
+            SelectedBook = ctx.Books.FirstOrDefault(s => s.Id == 1);
+            SelectedLibrary = LibraryCollectionViewSource.View.CurrentItem as Library;
+        }
+
+        public void Refresh()
+        {
+            
+            ctx = new LibraryEntities();
+            ctx.Libraries.Load();
+            ctx.Books.Load();
+            
+
+            LibraryCollectionViewSource.Source = new ObservableCollection<Library>(ctx.Libraries.ToList());
+            LibraryCollectionViewSource.SortDescriptions.Add(new SortDescription("Name", ListSortDirection.Ascending));    
+        }
+    }
+}
