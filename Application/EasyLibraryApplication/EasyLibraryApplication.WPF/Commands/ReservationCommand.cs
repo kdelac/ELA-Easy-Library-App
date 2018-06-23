984a033d39e3d253aadc91fcb74e3cbfb98586ce
Kristijan Mihaljinac <kristijan1mihaljinac@outlook.com>
2018-06-23 18:48:55 +0200

    Dodan dll za stvaranje qr koda

---
+++ Application/EasyLibraryApplication/EasyLibraryApplication.WPF/Commands/ReservationCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using EasyLibraryApplication.WPF.ViewModel;
+
+namespace EasyLibraryApplication.WPF.Commands
+{
+    class ReservationCommand : ICommand
+    {
+        private ReservationViewModel reservationViewModel;
+
+        public ReservationCommand(ReservationViewModel vm)
+        {
+            reservationViewModel = vm;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            reservationViewModel.SendEmail();
+            reservationViewModel.SaveReservationToDatabase();
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
