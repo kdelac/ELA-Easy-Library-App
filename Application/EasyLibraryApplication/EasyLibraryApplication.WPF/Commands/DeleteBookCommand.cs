7f9a78945c0b16020a0afd4ca58e2a5934e7eb42
Kristijan Mihaljinac <kristijan1mihaljinac@outlook.com>
2018-07-04 21:08:10 +0200

    Izrađena klasa za naredbu brisanja knjiga

---
+++ Application/EasyLibraryApplication/EasyLibraryApplication.WPF/Commands/DeleteBookCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using EasyLibraryApplication.WPF.ViewModel;
+
+namespace EasyLibraryApplication.WPF.Commands
+{
+    class DeleteBookCommand : ICommand
+    {
+        private CRUDBooksViewModel viewModel;
+        public DeleteBookCommand(CRUDBooksViewModel vm)
+        {
+            this.viewModel = vm;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+           viewModel.DeleteSelectedBook();
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
