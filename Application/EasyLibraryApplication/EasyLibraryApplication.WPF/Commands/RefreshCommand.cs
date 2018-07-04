60a2a62b625b57adf9f494bf55276bef06258ccd
Kristijan Mihaljinac <kristijan1mihaljinac@outlook.com>
2018-07-04 21:08:58 +0200

    Klasa za naredbu osvježavanja pogleda

---
+++ Application/EasyLibraryApplication/EasyLibraryApplication.WPF/Commands/RefreshCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using EasyLibraryApplication.WPF.View;
+using EasyLibraryApplication.WPF.ViewModel;
+
+namespace EasyLibraryApplication.WPF.Commands
+{
+    class RefreshCommand : ICommand
+    {
+        private CRUDBooksViewModel viewModel;
+
+        public RefreshCommand(CRUDBooksViewModel vm)
+        {
+            this.viewModel = vm;
+        }
+
+        public void Execute(object parameter)
+        {
+            viewModel.Refresh();
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
